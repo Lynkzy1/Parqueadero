ad172618c1662f6a684cbfa5447c356d5344d26b
Lynkzy1 <pmedagl87645@universidadean.edu.co>
2025-08-09 20:08:32 -0500

    Create Automovil.java

---
+++ Automovil.java
@@ -0,0 +1,11 @@
+public class Automovil extends Vehiculo {
+    private String tipoCombustible;
+
+    public Automovil(String placa, String marca, String modelo, LocalDateTime horaEntrada, String tipoCombustible) {
+        super(placa, marca, modelo, horaEntrada);
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public String getTipoCombustible() { return tipoCombustible; }
+    public void setTipoCombustible(String tipoCombustible) { this.tipoCombustible = tipoCombustible; }
+}
