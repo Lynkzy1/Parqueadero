ad559d709515a3f946250c305b957325f8800b55
Lynkzy1 <pmedagl87645@universidadean.edu.co>
2025-08-09 20:09:01 -0500

    Create Motocicleta.java

---
+++ Motocicleta.java
@@ -0,0 +1,11 @@
+public class Motocicleta extends Vehiculo {
+    private int cilindraje;
+
+    public Motocicleta(String placa, String marca, String modelo, LocalDateTime horaEntrada, int cilindraje) {
+        super(placa, marca, modelo, horaEntrada);
+        this.cilindraje = cilindraje;
+    }
+
+    public int getCilindraje() { return cilindraje; }
+    public void setCilindraje(int cilindraje) { this.cilindraje = cilindraje; }
+}
